e19a89a42256f23aeab9f644e401eea8c8b61592
Mark Grand <mgrand@markgrand.com>
2021-09-04 17:21:42 -0400

    File logic

--- tszero.go
+++ tszero.go
@@ -2,8 +2,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 const (
@@ -17,14 +21,20 @@ var format string
 // If true, the help flag was specified.
 var help bool
 
+// if true, the v (verbose) flag was specified
+var verbose bool
+
 // The name of the archive file to process or empty string to indicate processing the stdin
 var fileName string
 
 // Set up the command line parsing
 func init() {
+	log.SetOutput(os.Stderr)
 	flag.StringVar(&format, "format", "", "The value of format must be tar or zip.")
 	flag.BoolVar(&help, "help", false, "Specify this to see the help message.")
+	flag.BoolVar(&verbose, "v", false, "Verbose")
 	flag.Parse()
+	fileName = flag.Arg(0)
 }
 
 // Print the help message.
@@ -34,28 +44,58 @@ func printHelp() {
 }
 
 // Handle a tar archive
-func doTar() {
+func doTar(writer io.Reader) {
 
 }
 
 // Handle a zip archive
-func doZip() {
+func doZip(writer io.Reader) {
 
+}
+
+// get the reader that we will use to read the archive.
+func getReader() io.Reader {
+	if len(fileName) == 0 {
+		logMaybe("Reading from stdin")
+		return os.Stdin
+	}
+	logMaybe("File name is " + fileName)
+	file, err := os.Open("file.go") // For read access.
+	if err == nil {
+		return bufio.NewReader(file)
+	}
+	log.Fatal(err)
+	//goland:noinspection GoUnreachableCode
+	return nil // will never get here, but the compiler does not realize that os.Exit doesn't return
+}
+
+//Top level archive processing
+func processArchive(processor func(writer io.Reader)) {
+	reader := getReader()
+	processor(reader)
 }
 
 //main entry point into
 func main() {
+	logMaybe("tszero starting")
 	if help {
 		printHelp()
 	} else {
 		switch format {
 		case tarFmt:
-			doTar()
+			processArchive(doTar)
 		case zipFmt:
-			doZip()
+			processArchive(doZip)
 		default:
+			// TODO auto-detect type of archive file and make the format flag optional.
 			printHelp()
 		}
 	}
-	fmt.Println("tszero: ", help)
+	logMaybe("tszero finished")
+}
+
+func logMaybe(msg string) {
+	if verbose {
+		log.Println(msg)
+	}
 }
